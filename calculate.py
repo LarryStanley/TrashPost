515ab5d6da64b9745e8733a04726d0d169bfca36
LarryStanley <kill.xmurderer@gmail.com>
2015-07-02 23:52:35 +0800

    Update.

--- calculate.py
+++ calculate.py
@@ -6,8 +6,20 @@ with open("trashData.json") as jsonFile:
     allData = json.load(jsonFile)
     likeCount = 0
     commentCount = 0
+    timeCount = []
+    dateCount = []
     for post in allData:
         likeCount += int(post['likes']['summary']['total_count'])
         commentCount += int(post['comments']['summary']['total_count'])
+        #about created time
+        createdTime = post['created_time']
+        createdTime = createdTime.split("T")
+        if str(createdTime[0]) in dateCount:
+            dateCount[str(createdTime[0])]++
+        else:
+            dateCount[str(createdTime[0])] = 1
+
+
     print likeCount
     print commentCount
+    print dateCount
