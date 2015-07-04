6f41fe7c4368c4241b2431fe6a4c41b51926ecfa
LarryStanley <kill.xmurderer@gmail.com>
2015-07-04 19:06:20 +0800

    Update.

--- calculate.py
+++ calculate.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import json
 import urllib2
 import sys
@@ -6,20 +7,44 @@ with open("trashData.json") as jsonFile:
     allData = json.load(jsonFile)
     likeCount = 0
     commentCount = 0
+    allMessage = ''
     timeCount = []
-    dateCount = []
+    dateCount = {}
+    hourCount = {}
     for post in allData:
+        
+        if 'message' in post:
+            allMessage += post['message']
+
         likeCount += int(post['likes']['summary']['total_count'])
         commentCount += int(post['comments']['summary']['total_count'])
+
         #about created time
         createdTime = post['created_time']
         createdTime = createdTime.split("T")
+
+        #date count
         if str(createdTime[0]) in dateCount:
             dateCount[str(createdTime[0])] = dateCount[str(createdTime[0])] + 1
         else:
             dateCount[str(createdTime[0])] = 1
 
+        #time count
+        createdHour = createdTime[1].split(":")[0]
+        createdHour = int(createdHour)
+        if createdHour in hourCount:
+            hourCount[createdHour] = hourCount[createdHour] + 1
+        else:
+            hourCount[createdHour] = 1
 
-    print likeCount
-    print commentCount
+    #sort the result
+    dateCount = sorted(dateCount.iteritems(), key=lambda d:d[1], reverse = True)
+    hourCount = sorted(hourCount.iteritems(), key=lambda d:d[1], reverse = True)
+
+    print "{}:{}".format("letter", len(allMessage))
+    print len(allMessage)/len(allData)
+    print "{}:{}".format("Post count", len(allData))
+    print "{}:{}".format("Likes",likeCount)
+    print "{}:{}".format("Comments",commentCount)
     print dateCount
+    print hourCount
